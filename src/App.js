9cde11d6b36361725d8bee62ac1d02f4042b2d58
Jandomen <alquizor8@gmail.com>
2024-05-01 22:30:44 -0600

    primera parte de la tienda

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1> ecommerce_frontend </h1>
+
+        <p>
+          @Derechos Reservados j@ndomen.com
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
